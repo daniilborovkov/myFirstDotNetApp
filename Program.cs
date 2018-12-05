226e55dd4e5ff0e6f95d039a2491be0e5c8b2277
Daniil Borovkov <daniilborowkow@gmail.com>
2018-12-05 13:20:54 +0600

    First steps

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace myFirstDotNetApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Second line");
         }
     }
 }
